a591648282f6a0dc584709334d51eac2f674aa47
cesarg01 <gbrothers93@gmail.com>
2017-11-17 18:29:12 -0800

    Refactor
    Clean the code up a bit.

--- comma_pic_grid.py
+++ comma_pic_grid.py
@@ -1,4 +1,4 @@
-# 
+# Chapter 4 Practice Projects from Automate the Boring Stuff With Python.
 def comma(list):
     """
     # Take a list of values and returns a string with all the items separated by a comma and a space, with and inserted
@@ -11,6 +11,19 @@ def comma(list):
         else:
             words = words + list[i] + ', '
     return words
+
+
+def printer(grid):
+    """
+    # Take a grid and printed a certain way.
+    """
+    cols = len(grid[0])
+    rows = len(grid)
+
+    for i in range(cols):
+        for j in range(rows):
+            print(grid[j][i], end='')
+        print()
 
 spam = ['apples', 'bananas', 'tofu', 'cats', 'berry']
 print(comma(spam))
@@ -26,10 +39,4 @@ grid = [['.','.','.','.','.','.'],
         ['.','.','.','.','.','.'],
 ]
 
-cols = len(grid[0])
-rows = len(grid)
-
-for i in range(cols):
-    for j in range(rows):
-        print(grid[j][i], end='')
-    print()
+printer(grid)
